df3e21e40f0c4245090f16f5b5d060e970d97c7c
James Andrews <jbandrew14@gmail.com>
2022-01-04 20:30:56 -0500

    user making POST request to create new book is set as the added_by field for Book; added list view for featured books

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+from typing import List
 from django.db.models.base import Model
 from django.shortcuts import render
 from .models import Book, Author
-from rest_framework.viewsets import ModelViewSet
 from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
 
 class BookViewSet(ModelViewSet):
@@ -12,9 +14,17 @@ class BookViewSet(ModelViewSet):
     def perform_create(self, serializer):
         author_name = self.request.data['author']
         author = Author.objects.get(name=author_name)
-        serializer.save(author=author)
+        serializer.save(author=author, added_by=self.request.user)
 
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+
+class FeaturedBooksListView(ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = Book.objects.filter(featured=True)
+        return queryset
