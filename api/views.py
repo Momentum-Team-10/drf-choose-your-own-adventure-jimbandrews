d854ccf7cb72170c22325191466b9a7546fd22d0
James Andrews <jbandrew14@gmail.com>
2022-01-04 19:46:17 -0500

    trying to add author properly in views

--- api/views.py
+++ api/views.py
@@ -9,6 +9,12 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def perform_create(self, serializer):
+        breakpoint()
+        author_name = self.request.data['author']
+        author = Author.objects.get(name=author_name)
+        serializer.save(author=author)
+
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
